dfa20a3bc0b4b9b1115b65ace063be4a1485a405
admin <stefanivanovc@gmail.com>
2023-01-21 13:40:02 +0200

    Add new project

---
+++ Influenstar/Influenstar/Controllers/PostsController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Influenstar.Controllers
+{
+    public class PostsController : Controller
+    {
+        [HttpPost]
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
---
+++ Influenstar/Influenstar/Models/Posts/CreatePostModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Influenstar.Models.Posts
+{
+    public class CreatePostModel
+    {
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+
+    }
+}
